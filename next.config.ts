f03b5e58659684d6c93bd1b5f1d94d5ba56f3f60
sravan kumar <sravan-yandapalli>
2025-04-21 01:20:54 +0530

    summer\

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    MY_AWS_REGION: process.env.MY_AWS_REGION,
+    DYNAMODB_CAMP_TABLE: process.env.DYNAMODB_CAMP_TABLE,
+    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME,
+  },
 };
 
 export default nextConfig;
