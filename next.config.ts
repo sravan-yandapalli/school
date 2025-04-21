7a4d84aa4ca692f5634ee3c9b9be5fec313478dc
sravan kumar <sravan-yandapalli>
2025-04-21 11:35:37 +0530

    jd

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  env: {
-    MY_AWS_REGION: process.env.MY_AWS_REGION,
-    DYNAMODB_CAMP_TABLE: process.env.DYNAMODB_CAMP_TABLE,
-    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME,
-  },
+  output: "standalone", // Required for server functions (API routes) in Amplify
+  reactStrictMode: true, // Optional but recommended
 };
 
 export default nextConfig;
